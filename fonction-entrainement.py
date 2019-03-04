bc8a5ebf068d8a64bd94e0f19d05ab50b1730fef
DPICART <david.picart@outlook.fr>
2019-03-04 21:57:49 +0100

    Modif script fonction

--- fonction-entrainement.py
+++ fonction-entrainement.py
@@ -1,68 +1,81 @@
+montrerSolution = False
+
 # Donner la valeur de retour de cette fonction
 def fonctionA():
     return 1
 
-#print("fonctionA():",fonctionA())
+if montrerSolution:
+    print("fonctionA():",fonctionA())
 
 # Donner la valeur de retour de cette fonction
 def fonctionB():
     return 2
 
-#print("fonctionB():",fonctionB())
+if montrerSolution:
+    print("fonctionB():",fonctionB())
 
 # Donner la valeur de retour de cette fonction
 def fonctionC():
     return 3
 
-#print("fonctionC():",fonctionC())
+if montrerSolution:
+    print("fonctionC():",fonctionC())
 
 # Donner la valeur de retour de cette fonction
 def fonctionD():
     return fonctionA()+fonctionB()-fonctionC()
 
-#print("fonctionD():",fonctionD())
+if montrerSolution:
+    print("fonctionD():",fonctionD())
 
 # Donner la valeur de retour de cette fonction
 def fonctionE():
     return fonctionB()+fonctionD()
 
-#print("fonctionE():",fonctionE())
+if montrerSolution:
+    print("fonctionE():",fonctionE())
 
 # Donner la valeur de retour de cette fonction en passant 5 en paramètre
 def fonctionF( command ):
     return command * fonctionA()
 
-#print("fonctionF():",fonctionF(5))
+if montrerSolution:
+    print("fonctionF():",fonctionF(5))
 
 # Donner la valeur de retour de cette fonction en passant 2 en paramètre
 def fonctionG( et ):
     return et**2
 
-#print("fonctionG():",fonctionG(2))
+if montrerSolution:
+    print("fonctionG():",fonctionG(2))
 
 # Donner la valeur de retour de cette fonction en passant 3 en paramètre
 def fonctionH( conquer ):
     return conquer + fonctionG(conquer) + fonctionF( conquer )
 
-#print("fonctionH():",fonctionH(3))
+if montrerSolution:
+    print("fonctionH():",fonctionH(3))
 
 # Donner la valeur de retour de cette fonction en passant 10 en paramètre
 def fonctionI( aleatoire ):
     return aleatoire + fonctionH( aleatoire ) + fonctionF( fonctionG( aleatoire) )
 
-#print("fonctionI():",fonctionI(10))
+if montrerSolution:
+    print("fonctionI():",fonctionI(10))
 
 # Donner la valeur de retour de cette fonction en passant 64 et 19 en paramètre
 def fonctionJ( b , a):
     return [ a , b ]
 
-#print("fonctionJ():",fonctionJ(64, 19))
+if montrerSolution:
+    print("fonctionJ():",fonctionJ(64, 19))
     
 # Donner la valeur de retour de cette fonction en passant 9.5 et 30.5 en paramètre
 def fonctionK( b , a):
     return fonctionJ( 2*a, 2*b)
 
-#print("fonctionK():",fonctionK(9.5, 30.5))
+if montrerSolution:
+    print("fonctionK():",fonctionK(9.5, 30.5))
 
 # Donner la valeur de retour de cette fonction en passant 5 en parametre
 def fonctionL( a ):
@@ -70,4 +83,5 @@ def fonctionL( a ):
         return 0
     return a + fonctionL(a - 1)
 
-print("fonctionL():",fonctionL(5))
+if montrerSolution:
+    print("fonctionL():",fonctionL(5))
